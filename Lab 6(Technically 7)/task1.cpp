ec7d58b81012387ee12b2a93c0c82073c088ec31
RehanFastians <rehan14rs@gmail.com>
2025-10-16 14:50:33 +0500

    folder created

---
+++ Lab 6(Technically 7)/task1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+class Stack{
+    int *arr=nullptr;
+    int size=0;
+    public:
+    Stack (int size) : size(size){
+        arr = new int[size];
+    }
+};
+int main(){
+
+    return 0;
+}
