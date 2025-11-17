d811a3ee7c622bdeb84c736a8d38b19e8faa3dc6
RehanFastians <rehan14rs@gmail.com>
2025-11-17 15:03:54 +0500

    djfs

---
+++ Lab 11/task1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define f(i, s, e) for (int i = s; i < e; i++)
+#define fn(i, s, e) for (int i = s; i >= e; i--)
+#define nl cout << endl;
+
+using namespace std;
+
+class Node{
+    int key;
+    string value;
+    public:
+    Node(int key, string value) : key(key), value(value){}
+};
+
+
+int main()
+{
+
+    return 0;
+}
